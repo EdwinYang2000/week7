0fba3c32db49ce8c2e9165fd3e87ffd00f8f0c57
Edwin Yang <edwinymc@icloud.com>
2017-12-01 22:17:13 +0800

    update1

--- simpledu/manage.py
+++ simpledu/manage.py
@@ -1,7 +1,9 @@
 from simpledu.app import create_app
 
 app = create_app('development')
+app.config['SECRET_KEY'] = '123456'
 
 if __name__ == '__main__':
     app.run()
+
 
--- simpledu/simpledu/app.py
+++ simpledu/simpledu/app.py
@@ -1,21 +1,37 @@
 from flask import Flask,render_template
-
 from simpledu.config import configs
-from simpledu.models import db, Course
-from .handlers import front, course, admin, user
+from simpledu.models import db, User
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 
 def register_blueprints(app):
+    from .handlers import front, course, admin,user
     app.register_blueprint(front)
     app.register_blueprint(course)
     app.register_blueprint(admin)
     app.register_blueprint(user)
 
+
+def register_extensions(app):
+    db.init_app(app)
+    Migrate(app, db)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
+
 def create_app(config):
     """load different config according to imported config settings"""
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
-    db.init_app(app)
+    register_extensions(app)
     register_blueprints(app)
     return app
 
